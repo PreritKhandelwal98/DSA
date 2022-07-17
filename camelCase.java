ca1515117889dc5d60291c060564d14cc7ea2406
Prerit khandelwal <102379304+PreritKhandelwal98@users.noreply.github.com>
2022-07-17 15:26:06 +0530

    Add files via upload
    This program is to check number of words in String separated by camelCase

---
+++ camelCase.java
@@ -0,0 +1,18 @@
+package String;
+import java.util.*;
+// import java.lang.*;
+public class camelCase {
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+        String s = sc.next();
+        int terms = 1;
+        for(int i=0;i<s.length();i++){
+            String temp = Character.toString(s.charAt(i));
+            if(temp == temp.toUpperCase()){
+                terms++;
+            }
+        }
+
+        System.out.print("Number of words in the string "+s+" is: "+terms);
+    }
+}
