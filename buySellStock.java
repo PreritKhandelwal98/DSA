3fe572f68ecee0b6085224ec0571bcc7efde2ad0
Prerit khandelwal <102379304+PreritKhandelwal98@users.noreply.github.com>
2022-08-28 16:43:45 +0530

    Create buySellStock.java
    This is a brute force approach to solve stock buy and sell problem.
    This approach take O(N^2) for time complexity and O(1) for space complexity.

---
+++ buySellStock.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+	    int arr[] = new int[n];
+	    
+	    for(int i=0;i<arr.length;i++){
+	        arr[i] = sc.nextInt();
+	    }
+	    
+	    int maxProfit = 0;
+	    int min = arr[0];
+	    for(int i=0;i<arr.length;i++){
+	        for(int j=0;j<arr.length;j++){
+	            int curProfit = arr[j] - arr[i];
+	            if(curProfit>maxProfit){
+	                maxProfit = curProfit;
+	            }
+	        }    
+	    }
+	    System.out.println("The Profit is :"+maxProfit);
+	}
+}
