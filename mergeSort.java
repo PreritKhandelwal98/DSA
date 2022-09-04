34bee5738121db61ffcb950d89d56e51ab7a4623
Prerit khandelwal <102379304+PreritKhandelwal98@users.noreply.github.com>
2022-09-04 19:03:58 +0530

    Add files via upload

--- mergeSort.java
+++ mergeSort.java
@@ -1,8 +1,44 @@
 package InJava.Sorting;
 import java.util.Scanner;
 public class mergeSort {  
-        static void mergeSort(int[] arr){
+        public static void conquer(int[] arr,int si ,int mid, int ei){
+                int b[] = new int[ei-si+1];
 
+                int idx1 = si;
+                int idx2 = mid+1;
+                int x = 0;
+
+                while(idx1<=mid && idx2<=ei){
+                        if(arr[idx1] <=arr[idx2]){
+                                b[x++] = arr[idx1++];
+                        }else{
+                                b[x++] = arr[idx2++];
+                        }
+                }
+
+                while(idx1 <= mid){
+                        b[x++] = arr[idx1++];
+                }
+
+                while(idx2 <= ei){
+                        b[x++] = arr[idx2++];
+                }
+
+                for(int i=0, j=si;i<b.length;i++,j++){
+                        arr[j] = b[i];
+                }
+
+        }
+        public static void devide(int[] arr,int si , int ei){
+                int mid = si + (ei-si)/2;
+
+                if(si>=ei){
+                        return;
+                }
+
+                devide(arr,si,mid);
+                devide(arr,mid+1,ei);
+                conquer(arr,si,mid,ei);
         }
     public static void main(String[] args) { 
                 Scanner sc = new Scanner(System.in); 
@@ -15,18 +51,19 @@ public class mergeSort {
                 }
                  
                  
-                System.out.println("Array Before Bubble Sort");  
+                System.out.println("Array Before Merge Sort");  
                 for(int i=0; i < arr.length; i++){  
                         System.out.print(arr[i] + " ");  
                 }  
                 System.out.println();  
                   
-                mergeSort(arr);//sorting array elements using bubble sort  
+                devide(arr,0,arr.length-1);
                  
-                System.out.println("Array After Bubble Sort");  
+                System.out.println("Array After Merge Sort");  
                 for(int i=0; i < arr.length; i++){  
                         System.out.print(arr[i] + " ");  
                 }  
+
                 sc.close();
    
         }  
