ce29e84e2881d859b57c578eb9143144f1a8f01c
Prerit khandelwal <102379304+PreritKhandelwal98@users.noreply.github.com>
2022-09-15 15:22:25 +0530

    Update and rename binarySearch.java to BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,6 +1,6 @@
 package InJava.Searching;
 import java.util.Scanner;
-public class binarySearch{
+public class BinarySearch{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the size of array: ");
