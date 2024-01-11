6597743c51d11f35b4083aaec26dae8666c88922
Prerit Khandelwal <prerit.web@gmail.com>
2024-01-11 17:15:22 +0530

    created toString method to print matrix

--- Graph/GraphRepresentation.java
+++ Graph/GraphRepresentation.java
@@ -17,12 +17,27 @@ public class GraphRepresentation {
         E++;
     }
 
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append(V+" vertices, "+E+" edges "+"\n");
+
+        for(int v=0;v<V;v++){
+            sb.append(v+": ");
+            for(int w:adjMatrix[v]){
+                sb.append(w+" ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         GraphRepresentation g = new GraphRepresentation(4);
         g.addEdge(0,1 );
         g.addEdge(1,2);
         g.addEdge(2,3);
         g.addEdge(3,0);
+        System.out.println(g);
 
     }
 }
